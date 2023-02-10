7b7eef11985bc85fc58f30298e8a6ebe9894255c
rzbz <121601325+rzbz@users.noreply.github.com>
2023-02-10 18:39:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+console.log("asd");
